98b4fc3a705da4232e32aa3bf66877970bf9b1c6
ironmask431 <ironmask431@gmail.com>
2025-10-18 17:19:51 +0900

    Initial commit

---
+++ src/main/java/kevin/hellorabbitmq/controller/HelloController.java
@@ -0,0 +1,13 @@
+package kevin.hellorabbitmq.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam String q) {
+        return "hello " + q;
+    }
+}
