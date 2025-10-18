f38e40234b9ec602b1de0f8d04f68bb7a02cf55f
ironmask431 <ironmask431@gmail.com>
2025-10-18 19:23:45 +0900

    메세지 발송 테스트 api

--- src/main/java/kevin/hellorabbitmq/step1/Sender.java
+++ src/main/java/kevin/hellorabbitmq/step1/Sender.java
@@ -1,16 +1,14 @@
 package kevin.hellorabbitmq.step1;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 // 메세지를 생성 -> 큐(hellorabbitmq)에 전달하는 프로듀서 역할
 @Component
+@RequiredArgsConstructor
 public class Sender {
     private final RabbitTemplate rabbitTemplate;
-
-    public Sender(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
 
     public void send(String message) {
         rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME, message);
