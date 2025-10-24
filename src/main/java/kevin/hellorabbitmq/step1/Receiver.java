504de3c2c3a97e266b74b7f55c37d5a0119d0f2e
ironmask431 <ironmask431@gmail.com>
2025-10-24 21:06:38 +0900

    주석해제

--- src/main/java/kevin/hellorabbitmq/step1/Receiver.java
+++ src/main/java/kevin/hellorabbitmq/step1/Receiver.java
@@ -1,11 +1,11 @@
-//package kevin.hellorabbitmq.step1;
-//
-//import org.springframework.stereotype.Component;
-//
-//// 큐로부터 메세지를 가져와 메세지를 처리하는 컨슈머 역할.
-//@Component
-//public class Receiver {
-//    public void receiveMessage(String message) {
-//        System.out.println("[#] Received: " + message);
-//    }
-//}
+package kevin.hellorabbitmq.step1;
+
+import org.springframework.stereotype.Component;
+
+// 큐로부터 메세지를 가져와 메세지를 처리하는 컨슈머 역할.
+@Component
+public class Receiver {
+    public void receiveMessage(String message) {
+        System.out.println("[#] Received: " + message);
+    }
+}
