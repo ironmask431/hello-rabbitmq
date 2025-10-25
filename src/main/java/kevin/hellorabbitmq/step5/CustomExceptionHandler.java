59415721b84ea0d2e6b28ee8e1a70b91e51fb7c1
ironmask431 <ironmask431@gmail.com>
2025-10-25 14:49:49 +0900

    STEP-6.Topic Exchange 예제

--- src/main/java/kevin/hellorabbitmq/step5/CustomExceptionHandler.java
+++ src/main/java/kevin/hellorabbitmq/step5/CustomExceptionHandler.java
@@ -1,36 +1,36 @@
-package kevin.hellorabbitmq.step5;
-
-import org.springframework.stereotype.Component;
-
-// 에러로그나 일반로그를 라우팅키를 지정하여 큐로 메세지 발행
-@Component
-public class CustomExceptionHandler {
-    private final LogPublisher logPublisher;
-
-    public CustomExceptionHandler(LogPublisher logPublisher) {
-        this.logPublisher = logPublisher;
-    }
-
-    // 에러나 로그 처리
-    public void handleException(Exception e) {
-        String message = e.getMessage();
-
-        String routingKey;
-
-        if (e instanceof NullPointerException) {
-            routingKey = "error";
-        } else if (e instanceof IllegalArgumentException) {
-            routingKey = "warn";
-        } else {
-            routingKey = "error";
-        }
-
-        logPublisher.publish(routingKey, "Exception이 발생했음 : " + message);
-    }
-
-    // 메시지 처리
-    public void handleMessage(String message) {
-        String routingKey = "info";
-        logPublisher.publish(routingKey, "Info Log : " + message);
-    }
-}
+//package kevin.hellorabbitmq.step5;
+//
+//import org.springframework.stereotype.Component;
+//
+//// 에러로그나 일반로그를 라우팅키를 지정하여 큐로 메세지 발행
+//@Component
+//public class CustomExceptionHandler {
+//    private final LogPublisher logPublisher;
+//
+//    public CustomExceptionHandler(LogPublisher logPublisher) {
+//        this.logPublisher = logPublisher;
+//    }
+//
+//    // 에러나 로그 처리
+//    public void handleException(Exception e) {
+//        String message = e.getMessage();
+//
+//        String routingKey;
+//
+//        if (e instanceof NullPointerException) {
+//            routingKey = "error";
+//        } else if (e instanceof IllegalArgumentException) {
+//            routingKey = "warn";
+//        } else {
+//            routingKey = "error";
+//        }
+//
+//        logPublisher.publish(routingKey, "Exception이 발생했음 : " + message);
+//    }
+//
+//    // 메시지 처리
+//    public void handleMessage(String message) {
+//        String routingKey = "info";
+//        logPublisher.publish(routingKey, "Info Log : " + message);
+//    }
+//}
--- src/main/java/kevin/hellorabbitmq/step5/RabbitMQConfig.java
+++ src/main/java/kevin/hellorabbitmq/step5/RabbitMQConfig.java
@@ -1,55 +1,55 @@
-package kevin.hellorabbitmq.step5;
-
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitMQConfig {
-    // 각 큐 이름을 정의
-    public static final String ERROR_QUEUE = "error_queue";
-    public static final String WARN_QUEUE = "warn_queue";
-    public static final String INFO_QUEUE = "info_queue";
-
-    // 익스체인지 명 정의
-    public static final String DIRECT_EXCHANGE = "direct_exchange";
-
-    // 익스체인지 빈등록
-    @Bean
-    public DirectExchange directExchange() {
-        return new DirectExchange(DIRECT_EXCHANGE);
-    }
-
-    // 각 큐를 빈등록
-    @Bean
-    public Queue errorQueue() {
-        return new Queue(ERROR_QUEUE, false);
-    }
-
-    @Bean
-    public Queue warnQueue() {
-        return new Queue(WARN_QUEUE, false);
-    }
-    @Bean
-    public Queue infoQueue() {
-        return new Queue(INFO_QUEUE, false);
-    }
-
-    // 익스체인지와 각 큐를 연결
-    // 익스체인지로 들어온 메세지는 "라우팅키" 에 따라 특정 큐로만 들어간다.
-    @Bean
-    public Binding errorBinding() {
-        return BindingBuilder.bind(errorQueue()).to(directExchange()).with("error");
-    }
-    @Bean
-    public Binding warnBinding() {
-        return BindingBuilder.bind(warnQueue()).to(directExchange()).with("warn");
-    }
-    @Bean
-    public Binding infoBinding() {
-        return BindingBuilder.bind(infoQueue()).to(directExchange()).with("info");
-    }
-}
+//package kevin.hellorabbitmq.step5;
+//
+//import org.springframework.amqp.core.Binding;
+//import org.springframework.amqp.core.BindingBuilder;
+//import org.springframework.amqp.core.DirectExchange;
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitMQConfig {
+//    // 각 큐 이름을 정의
+//    public static final String ERROR_QUEUE = "error_queue";
+//    public static final String WARN_QUEUE = "warn_queue";
+//    public static final String INFO_QUEUE = "info_queue";
+//
+//    // 익스체인지 명 정의
+//    public static final String DIRECT_EXCHANGE = "direct_exchange";
+//
+//    // 익스체인지 빈등록
+//    @Bean
+//    public DirectExchange directExchange() {
+//        return new DirectExchange(DIRECT_EXCHANGE);
+//    }
+//
+//    // 각 큐를 빈등록
+//    @Bean
+//    public Queue errorQueue() {
+//        return new Queue(ERROR_QUEUE, false);
+//    }
+//
+//    @Bean
+//    public Queue warnQueue() {
+//        return new Queue(WARN_QUEUE, false);
+//    }
+//    @Bean
+//    public Queue infoQueue() {
+//        return new Queue(INFO_QUEUE, false);
+//    }
+//
+//    // 익스체인지와 각 큐를 연결
+//    // 익스체인지로 들어온 메세지는 "라우팅키" 에 따라 특정 큐로만 들어간다.
+//    @Bean
+//    public Binding errorBinding() {
+//        return BindingBuilder.bind(errorQueue()).to(directExchange()).with("error");
+//    }
+//    @Bean
+//    public Binding warnBinding() {
+//        return BindingBuilder.bind(warnQueue()).to(directExchange()).with("warn");
+//    }
+//    @Bean
+//    public Binding infoBinding() {
+//        return BindingBuilder.bind(infoQueue()).to(directExchange()).with("info");
+//    }
+//}
