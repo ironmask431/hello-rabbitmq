5ff79c04dc244652a312ee972068fd450a837420
ironmask431 <ironmask431@gmail.com>
2025-11-06 21:48:04 +0900

    STEP.8 retryTemplate 관련 주석 정리

--- src/main/java/kevin/hellorabbitmq/step8/OrderConsumer.java
+++ src/main/java/kevin/hellorabbitmq/step8/OrderConsumer.java
@@ -18,19 +18,26 @@ public class OrderConsumer {
 
     @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE)
     public void consume(String message) {
+        // RetryConfig 에서 빈으로 등록한 retryTemplate 을 사용.
         retryTemplate.execute(context -> {
             try {
                 System.out.println("[Order Consumer] 리시브 메시지 : " + message + " # retry : " + context.getRetryCount());
                 // 실패 조건 (message 에 fail 포함될 경우)
                 if ("fail".equalsIgnoreCase(message)) {
+                    // exception발생 -> catch로 이동
                     throw new RuntimeException(message);
                 }
                 System.out.println("[Order Consumer] 메시지 처리 성공 " + message);
             } catch (Exception e) {
-                // 3회이상 재시도 실패 시 DLQ로 메세지 보냄.
                 if (context.getRetryCount() >= 2) {
+                    // 재시도 3회 이상일 시 DLQ로 메세지가 가게됨.
+                    System.out.println("[Order Consumer] DLQ로 메세지 보냄 " + message);
                     rabbitTemplate.convertAndSend(RabbitMQConfig.ORDER_TOPIC_DLX, RabbitMQConfig.DEAD_LETTER_ROUTING_KEY, message);
                 } else {
+                    // retryTemplate 내부에서 재시도 3회 미만일 때  예외를 던지면 (throw e)
+                    // retryTemplate이 catch하여 내부적으로 다시 재시도한다.
+                    // Spring AMQP나 RabbitMQ에 nack을 보내는건 아님. 내부적으로만 처리. (3회 재시도 후 DLQ로 메세지 이동)
+                    System.out.println("[Order Consumer] throw e.");
                     throw e;
                 }
             }
--- src/main/java/kevin/hellorabbitmq/step8/RetryConfig.java
+++ src/main/java/kevin/hellorabbitmq/step8/RetryConfig.java
@@ -6,18 +6,26 @@ import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
+/**
+ * RetryTemplate 의 속성을 정의하고 bean으로 등록함.
+ * RetryTemplate 은 execute() 내부에서 throw exception 발생 시 자체적으로 재시도하는 기능을 제공함.
+ * 주의 : RetryTemplate은 RabbitMQ 브로커 수준의 재전송(NACK 기반) 과는 별개입니다.
+ * → 즉, 컨슈머 코드 안에서 비즈니스 로직 재시도용입니다.
+ */
 @Configuration
 public class RetryConfig {
 
     @Bean
     public RetryTemplate retryTemplate() {
         RetryTemplate retryTemplate = new RetryTemplate();
+
         // 재시도 정책 설정: 최대 3번 시도
         SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
         retryPolicy.setMaxAttempts(3);
         // 백오프 정책 설정: 재시도 간격 1초
         FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
         backOffPolicy.setBackOffPeriod(1000L);
+
         retryTemplate.setRetryPolicy(retryPolicy);
         retryTemplate.setBackOffPolicy(backOffPolicy);
 
