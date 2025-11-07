f30c953b3c8e9843836f9c12cc466b0e592052ee
ironmask431 <ironmask431@gmail.com>
2025-11-07 12:58:04 +0900

    STEP-9. DB연동 메세지큐의 Transaction 처리

---
+++ src/main/java/kevin/hellorabbitmq/step9/MessageConsumer.java
@@ -0,0 +1,29 @@
+package kevin.hellorabbitmq.step9;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MessageConsumer {
+    private final StockRepository stockRepository;
+
+    public MessageConsumer(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @RabbitListener(queues = "transactionQueue")
+    public void receiveTransaction(StockEntity stockEntity) {
+        System.out.println("[MessageConsumer] received message  = " + stockEntity);
+        try {
+            stockEntity.setProcessed(true);
+            stockEntity.setUpdatedAt(LocalDateTime.now());
+            stockRepository.save(stockEntity); // 상태 업데이트
+            System.out.println("[MessageConsumer] StockEntity 저장 완료 ");
+        } catch (Exception e) {
+            System.out.println("[MessageConsumer] Entity 수정 에러 " + e.getMessage());
+            throw e; // todo 메시지를 데드레터 큐에 집어넣는다..
+        }
+    }
+}
---
+++ src/main/java/kevin/hellorabbitmq/step9/RabbitMQConfig.java
@@ -0,0 +1,31 @@
+package kevin.hellorabbitmq.step9;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public Queue queue() {
+        return new Queue("transactionQueue", true);
+    }
+
+    // 객체를 json으로 변환하여 rabbitmq message로 보내기 위한 용도
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter); // JSON 변환기 등록
+        rabbitTemplate.setChannelTransacted(true);  // 트랜잭션 활성화
+        return rabbitTemplate;
+    }
+}
